9ed0313de61d72cd7aeb8a5db948bc27228b4cb6
Vinícius <vinissfigueiroa@gmail.com>
2020-12-16 09:38:00 -0300

    Atividade concluída

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using metodos_construtores_2.classes;
+
+namespace metodos_construtores_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Utilizando o construtor vazio
+            Produto produto1 = new Produto();
+            produto1.Codigo = 1;
+            produto1.Nome = "Coca-Cola";
+            produto1.Descricao = "Refrigerante de cola";
+            produto1.Estoque = 30;
+
+            Console.WriteLine($"Produto 1\nCódigo: {produto1.Codigo}\nNome: {produto1.Nome}\nDescrição: {produto1.Descricao}\nEstoque: {produto1.Estoque}");
+
+            //Utilizando o construtor inserindo apenas o codigo
+            Produto produto2 = new Produto(10);
+            Console.WriteLine($"\nProduto 2\nCódigo: {produto2.Codigo}\nNome: {produto2.Nome ?? "null"}\nDescrição: {produto2.Descricao ?? "null"}\nEstoque: {produto2.Estoque}");
+
+            //Construtor inserindo todos os argumentos
+            Produto produto3 = new Produto(1, "Trakinas", "Bolacha de chocolate", 1000);
+            Console.WriteLine($"\nProduto 3\nCódigo: {produto3.Codigo}\nNome: {produto3.Nome}\nDescrição: {produto3.Descricao}\nEstoque: {produto3.Estoque}");
+        }
+    }
+}
---
+++ classes/Produto.cs
@@ -0,0 +1,26 @@
+using System;
+namespace metodos_construtores_2.classes
+{
+    public class Produto
+    {
+        public int Codigo{get; set;}
+        public string Nome{get; set;}
+        public string Descricao{get; set;}
+        public int Estoque{get; set;}
+
+        public Produto() {
+            
+        }
+
+        public Produto(int codigo) {
+            Codigo = codigo;
+        }
+
+        public Produto(int codigo, string nome, string descricao, int estoque) {
+            Codigo = codigo;
+            Nome = nome;
+            Descricao = descricao;
+            Estoque = estoque;
+        }
+    }
+}
